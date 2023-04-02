7ded55b4f2185a9bfc776cee8699d7a922a702a4
loki0101 <66391514+loki0101@users.noreply.github.com>
2023-04-02 13:27:10 -0300

    lógica OK, faltan detalles estéticos y que hacer funcionar botones

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,59 @@
 fetch('./assets/js/data.json')
     .then(response => response.json())
     .then(data => {
-        for (let i of data) {
-            console.log(i)
+        for (let element of data.jobs) {
+            console.log(element)
         }
-        showCards(data, cards)
+        showPortfolio(data.proyects, proyectsContainer)
+        showJobs(data.jobs, cards)
+        console.log(data.proyects)
     })
-    .catch(function(err){
+    .catch(function (err) {
         console.log(err);
     })
 
-    function showCards(array, container) {
-        const fragment = document.createDocumentFragment();
-        for (let element of array) {
-            container.innerHTML = '';
-            let div = document.createElement('div');
-            div.className = "jobs";
-            div.innerHTML = `
+function showJobs(array, container) {
+    const fragment = document.createDocumentFragment();
+    for (let element of array) {
+        container.innerHTML = '';
+        let div = document.createElement('div');
+        div.className = "jobs";
+        div.innerHTML = `
                 <h3>${element.puesto}</h3>
                 <p>${element.empresa}</p>
-                <div class="p-flex">
+                <div class="p-flex-card">
                     <p>${element.inicio}</p>
-                    <p>${element.fin}</p>
+                    <p> hasta </p>
+                    <p>${element.fin}</p>    
                 </div>
-                <p>${element.tareas}</p>
                 `
-                fragment.appendChild(div);
-        }
-        container.appendChild(fragment);
+        fragment.appendChild(div);
     }
+    container.appendChild(fragment);
+}
+
+//crear funcion q recorra las tech usadas y las agregue automaticamente
+
+function showPortfolio(array, container) {
+    const fragmentSnd = document.createDocumentFragment();
+    for (let element of array) {
+        container.innerHTML = '';
+        let div = document.createElement('div');
+        div.className = "proyects";
+        div.innerHTML = `
+                <img class="jobs-img" src=${element.image} alt="img">
+                <h3>${element.name}</h3>
+                <p>${element.date}</p>
+                <div class="p-flex">
+                    <span class="techs">${element.technologies[0]}</span>
+                    <span class="techs">${element.technologies[1]}</span>
+                    <span class="techs">${element.technologies[2]}</span>
+                    <span class="techs">${element.technologies[3]}</span>
+                </div>
+                <a class="a-details" href=${element.linkRepo} target="_blank">Details</a>
+                <p>${element.description}</p>
+                `
+        fragmentSnd.appendChild(div);
+    }
+    container.appendChild(fragmentSnd);
+}
